375d52908c2b2187a07bc550f4b86101011963c0
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-16 04:39:42 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"]
+function addElementTobeginningOfArray(array,element){
+    return newArray;
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+
+    return array;
+}
+
+
+function addElementToEndOfArray(array,element){}
+function destructivelyAddElementToEndOfArray(array,element){}
+function accessElementInArray(array,index){
+  return element;
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array;
+}
+function removeElementFromBeginningOfArray(array){
+  return array;
+}
+function removeElementFromEndOfArray(array){
+
+}
