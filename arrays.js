d261f990e0e4cefb22da8cefe74c54876024ed77
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-16 05:49:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -44,6 +44,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 function removeElementFromEndOfArray(array){
   newArray = array;
-  newArray.pop();
+  newArray.slice(-1);
   return newArray;
 }
