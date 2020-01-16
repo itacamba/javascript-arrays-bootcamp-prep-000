72765690fdca935a5cf676e67445ffbce21bd868
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-16 04:57:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"]
-function addElementToBeginningOfArray(array1,element){
-    var newArray = array1;
-    newArray.unshift(element)
+function addElementToBeginningOfArray(array,element){
+    var newArray = ${array};
+    newArray.unshift(${element})
     return newArray;
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
